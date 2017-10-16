203fcff38e9599cb21d956ebfbee84d0547ba77d
Greg Linch <glinch@mcclatchydc.com>
2017-10-16 16:41:57 -0400

    update name to McClatchy instead of MCC

--- src/locator.js
+++ src/locator.js
@@ -29,7 +29,7 @@ var Locator = function(options) {
     template: "REPLACE-DEFAULT-TEMPLATE",
 
     // Text
-    title: "MCC Locator",
+    title: "McClatchy Locator",
     footer: "Made by the <a href=\"//datanews.tumblr.com/\" target=\"_blank\">WNYC DataNews</a> team.  Locator only works in <a href=\"//google.com/chrome\" target=\"_blank\">Google Chrome</a> at the moment.  See how to <a href=\"//github.com/datanews/locator/\" target=\"_blank\">deploy Locator</a> for yourself or your organization.",
 
     // Main map
